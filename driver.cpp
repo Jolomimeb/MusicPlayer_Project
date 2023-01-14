0ab90c845bdec6cfd4e0ca261b77624c801d2751
Jolomimeb <111466708+Jolomimeb@users.noreply.github.com>
2023-01-14 03:38:32 -0500

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -5,21 +5,13 @@
 #include "Song.h" //Loads the Songs
 
 //Takes in an argument when you call this program
-//For example, if you type ./proj3 proj3_music.txt it will automatically pass proj3_music.txt in
-//as argv[1]. So it is required.
-int main (int argc, char* argv[]) {
-  string musicFile;
-  cout << "Welcome to UMBC Music Player"<<endl;
-  if(argc > 1){
-    musicFile = argv[1]; //stores the first argument as musicFile
-  } else{
-    cout << "One music files required - try again" << endl;
-    cout << "./proj3 proj3_music.txt" << endl;
+int main() {
+    string musicFile = "proj3_music.txt";
+    cout << "Welcome to Music Player" << endl;
+    
+    MusicPlayer* myMusic = new MusicPlayer(); //Dynamically allocates a new music player
+    myMusic->LoadCatalog(musicFile); //Loads the catalog
+    myMusic->MainMenu(); //Calls MainMenu
+    delete myMusic; //Deallocates the music player
     return 0;
-  }
-  MusicPlayer* myMusic = new MusicPlayer(); //Dynamically allocates a new music player
-  myMusic->LoadCatalog(musicFile); //Loads the catalog
-  myMusic->MainMenu(); //Calls MainMenu
-  delete myMusic; //Deallocates the music player
-  return 0;
 }
